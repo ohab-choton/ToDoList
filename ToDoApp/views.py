16de30dbb5f1eae3ab944fd04b0966701b7af6ce
Abdul Ohab <ohabchoton@gmail.com>
2025-01-01 00:19:11 +0600

    sent data from view sites

--- ToDoApp/views.py
+++ ToDoApp/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render ,redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .models import ToDo
 
 # Create your views here.
 def home(request):
+    if request.method == 'POST':
+        task=request.POST.get('task')
+        new_todo=ToDo(user=request.user,todo_name=task)
+        new_todo.save()
     return render(request, 'todoapp/todo.html')
 
 def register(request):
